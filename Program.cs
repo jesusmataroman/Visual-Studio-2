adbd1d7dba93ffb154045dd5d12570ac0b9f5aee
jesusmataroman <84407967+jesusmataroman@users.noreply.github.com>
2021-05-18 22:20:26 +0200

    Ejercicio 1 de Visual Studio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Alicacion_de_Consola_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(); //Para la ejecucion cuando se pulse cualquier tecla.
+        }
+    }
+}
